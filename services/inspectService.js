aae83d716a89cc6058ddcc31bfc4ece63b51d681
nischay30 <nischay.goyal@gmail.com>
2017-01-20 15:12:57 +0530

    Replicas added in compose file

--- services/inspectService.js
+++ services/inspectService.js
@@ -6,7 +6,7 @@ function inspectService (stackName, serviceNameToExpose, domainName, callback) {
 	let stackServiceName = stackName + '_' + serviceNameToExpose;
 	let serviceinfo = docker.getService(stackServiceName);
 	serviceinfo.inspect((err, data) => {
-		let redirectUrl = 'http://localhost:' + data.Endpoint.Ports[0].PublishedPort;
+		let redirectUrl = 'http://127.0.0.1:' + data.Endpoint.Ports[0].PublishedPort;
 		callback(null, domainName, redirectUrl);
 	});
 }
